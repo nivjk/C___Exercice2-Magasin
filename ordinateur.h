19c15cefbad265e10db6938e9276d6542aaee844
nivjk <134269336+nivjk@users.noreply.github.com>
2023-05-25 01:51:06 +0200

    Add files via upload

---
+++ ordinateur.h
@@ -0,0 +1,11 @@
+#ifndef ORDINATEUR_H
+#define ORDINATEUR_H
+
+// Structure pour représenter un ordinateur
+typedef struct {
+    char marque[20];
+    char modele[20];
+    int stock;
+} Ordinateur;
+
+#endif  // ORDINATEUR_H
